0d0668ea1df2127e6a3c205fefb170090d0ae4cd
Kichiryoku <albertjshederay@gmail.com>
2025-10-20 14:28:24 +0800

    CUSTOM COLOR IN DISPLAY TEXT(HEX CODE)

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,7 @@ var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
-{"name":"CustomTextColor","status":false,"description":"[v1.2] 自定义文字颜色插件：支持 \\rgb[r,g,b]、\\hex[#rrggbb]、\\rgbreset","parameters":{}}
+{"name":"CustomTextColor","status":false,"description":"突破32种预设颜色限制，支持用RGB/HEX值自定义文本颜色","parameters":{}},
+{"name":"Trb_TextColor","status":true,"description":"A plugin that allows text color to be selected from Hex Color Code.","parameters":{}},
+{"name":"PreviewColorSupport","status":false,"description":"简洁版预览颜色支持","parameters":{}}
 ];
